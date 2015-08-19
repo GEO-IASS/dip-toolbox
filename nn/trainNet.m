e037f39daacb9273e0bf272c9ec09fe0ca218bc0
Honza Blazek <gimli@matfyz.cz>
2015-08-19 23:21:07 +0200

    added testing tool for determination of NN size and Training set size

---
+++ nn/trainNet.m
@@ -0,0 +1,31 @@
+function trainNet(allBandsImg, roi, trainingSetSize, layers);
+% cilem funkce je zjistit velikost trenovaci mnoziny, kdy uz aproximace
+% dava pozadovane vysledky, taktez lze testovat velikost site
+
+% argumenty: linearizovany obrazek, vystupni obrazek, velikost trenovaci
+% mnoziny a velikost site
+
+allBandsImgLin = reshape(allBandsImg, size(allBandsImg, 1) * size(allBandsImg, 2), size(allBandsImg,3));
+
+imgSize = size(allBandsImgLin, 1);
+bands = size(allBandsImgLin, 2);
+
+subset = int32(rand(trainingSetSize,1) * imgSize);
+ins = allBandsImgLin(subset, 1:16);
+outs = allBandsImgLin(subset, 17:bands);
+
+net = feedforwardnet(layers, 'trainscg');
+net = configure(net, 'inputs', ins');
+net = configure(net, 'outputs', outs');
+net = train(net, ins', outs');
+
+roiLin = reshape(allBandsImg(roi(1):roi(2), roi(3):roi(4), 1:16), (roi(2) - roi(1) + 1) * (roi(4) - roi(3) + 1), 16);
+out = net(roiLin');
+
+preview = reshape(out', roi(2) - roi(1) + 1, roi(4) - roi(3) + 1, bands - 16); 
+
+filename = ['leonardoPreview-', num2str(trainingSetSize), '-', num2str(layers(1)), 'x', num2str(layers(2)), '.png'];
+
+imwrite(preview(:,:,10), filename);
+
+end
