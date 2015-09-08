71c29cc1cf2bcd4fec0bd89e4267536ce493d7b2
Honza Blažek <gimli@matfyz.cz>
2015-09-08 15:55:26 +0200

    fixed name of variable - phantom already taken

--- phantom/computeANN.m
+++ phantom/computeANN.m
@@ -3,13 +3,13 @@ function computeANN( phantomID, M, layersID)
 % Function takes one phantom from directory and train one FF-ANN. Results 
 % (error vectors and network itself) stores into ouput file.
 
-cd '/storage/ostrava1/home/gimli/dip-toolbox/phantom';
+%cd '/storage/ostrava1/home/gimli/dip-toolbox/phantom';
 
 phantomName = ['phantom-m', num2str(M), '-', num2str(phantomID)];
-load(phantomName);
+data = load(phantomName);
 load('layersStore');
 
-[err, net, perf] = estimateNirBands(phantom, layers{layersID}); 
+[err, net, perf] = estimateNirBands(data.phantom, layers{layersID}); 
 
 filename = ['output-P', num2str(phantomID), '-M', num2str(M), '-L', num2str(layersID)];
 
