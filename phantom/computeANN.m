39d345d07d4a7c8d4457ecc9e2a26da4e0c04672
Honza Blažek <gimli@matfyz.cz>
2015-09-08 15:09:22 +0200

    updates for full results

---
+++ phantom/computeANN.m
@@ -0,0 +1,19 @@
+function computeANN( phantomID, M, layersID)
+% computeANN(phantomID, M, layersID) - function for cesnet computing. 
+% Function takes one phantom from directory and train one FF-ANN. Results 
+% (error vectors and network itself) stores into ouput file.
+
+cd '/storage/ostrava1/home/gimli/dip-toolbox/phantom';
+
+phantomName = ['phantom-m', num2str(M), '-', num2str(phantomID)];
+phantom = load(phantomName);
+layers = load(layersStore);
+
+[err, net] = estimateNirBands(phantom.phantom, layers(layersID)); 
+
+filename = ['output-P', num2str(phantomID), '-M', num2str(M), '-L', num2str(layersID)];
+
+save(filename, 'err', 'net');
+
+end
+
--- phantom/estimateNirBands.m
+++ phantom/estimateNirBands.m
@@ -1,19 +1,27 @@
-function [err, net] = estimateNirBands(dataset, nnLayers)
+function [err, net, perf] = estimateNirBands(dataset, nnLayers, trainSetSize)
 % estimateNirBands(dataset, nnLayers) creates neural network which 
 % estimates from first 16 dimensions of dataset.work(:,:,1:16) next 16 
 % dimensions of dataset.work(:,:,17:32). As a result we obtain computed estimation error on 
 % dataset.clean, trained neural network and 
+
+if (nargin < 3)
+    trainSetSize = size(dataset.work,1) * size(dataset.work,2);
+end
 
 height = size(dataset.work,1);
 width = size(dataset.work,2);
 ins = reshape(dataset.work(:,:,1:16), width * height, 16);
 outs = reshape(dataset.work(:,:,17:32), width * height, 16);
 
+subset = int64(rand(trainSetSize, 1) * (size(dataset.work,1) * size(dataset.work,2) - 1)) + 1;
+input = ins(subset, :);
+output = outs(subset, :);
+
 net = feedforwardnet(nnLayers, 'trainscg');
-net = configure(net, 'inputs', ins');
-net = configure(net, 'outputs', outs');
+net = configure(net, 'inputs', input');
+net = configure(net, 'outputs', output');
 net.trainParam.epochs = 1000;
-net = train(net, ins', outs', 'useGPU', 'yes');
+[net, perf] = train(net, input', output', 'useGPU', 'yes');
 
 oneLayer = reshape(dataset.clean(1:height/12:height, 1, 1:16), 12, 16);
 twoLayers = reshape(dataset.clean(1:height/12:height, width, 1:16), 12, 16);
