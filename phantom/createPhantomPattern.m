dd94551ba3286065ae81c5e781883f92c602a58a
Honza Blazek <gimli@matfyz.cz>
2015-09-11 23:58:40 +0200

    fixed material count in pattern creation

--- phantom/createPhantomPattern.m
+++ phantom/createPhantomPattern.m
@@ -1,5 +1,6 @@
-function phantomPattern = createPhantomPattern(segmentRows, maxWidth, materialsCount, underdrawingsCoverage)
-% createPhantomPattern(segmentRows, maxWidth, materialsCount, underdrawingsCoverage) creates 2D matrix where each pixel contains id of
+function phantomPattern = createPhantomPattern(segmentRows, maxWidth, usedMaterials, materialsCount, underdrawingsCoverage)
+% createPhantomPattern(segmentRows, maxWidth, usedMaterials, materialsCount, 
+% underdrawingsCoverage) creates 2D matrix where each pixel contains id of
 % material. This function creates horizontal stripes. Each stripe
 % represents one used material. On the right side (according to specified
 % underdrawingCoverage) simulation of sandwich with underdrawings is
@@ -11,10 +12,10 @@ underdrawingCols = floor(maxWidth * underdrawingsCoverage);
 cols = int16(underdrawingCols / underdrawingsCoverage);
 
 % allocate output pattern
-phantomPattern = zeros(segmentRows * materialsCount, cols);
+phantomPattern = zeros(segmentRows * usedMaterials, cols);
 
 row = 1;
-for m=1:materialsCount
+for m=1:usedMaterials
     phantomPattern(row:row+segmentRows-1, 1:cols - underdrawingCols) = m;
     phantomPattern(row:row+segmentRows-1, cols - underdrawingCols + 1:cols) = m + materialsCount;
     row = row + segmentRows;
