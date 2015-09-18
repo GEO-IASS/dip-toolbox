5837d360193428f1cc1839288058da786033e9a3
Honza Blažek <gimli@matfyz.cz>
2015-09-18 20:13:36 +0200

    more layers for NN network, next test

---
+++ phantom/computeANN5.m
@@ -0,0 +1,38 @@
+function computeANN5( M, layers, fileID )
+% computeANN(phantomID, M, layersID,fileID) - function for cesnet computing. 
+% Function takes one phantom from directory and train one FF-ANN. Results 
+% (error vectors and network itself) stores into ouput file.
+
+cd '/storage/ostrava1/home/gimli/dip-toolbox/phantom';
+
+load('materials-firenze.mat');
+pattern = createPhantomPattern(int16(500/M), 500, M, 120, 0.02);
+permutation = randperm(120);
+mStatsPerm(1:120,:,:) = mStats(permutation, :, :); % mStats is loaded from materials-firenze
+mStatsPerm(121:240,:,:) = mStats(120+permutation, :, :);
+phantom = createPhantom(pattern, mStatsPerm);
+
+nnLayers = ones(layers, 1) * 50;
+
+height = size(phantom.clean,1);
+width = size(phantom.clean,2);
+
+trainSetSize = min(height*width, 10^5);
+
+[net, perf] = trainANN(phantom.work, nnLayers, trainSetSize); 
+
+oneLayer = reshape(phantom.clean(1:height, 1, 1:16), height, 16);
+twoLayers = reshape(phantom.clean(1:height, width, 1:16), height, 16);
+
+oneLayerNir = net(oneLayer')';
+twoLayersNir = net(twoLayers')';
+
+err.oneLayer = (oneLayerNir - reshape(phantom.clean(1:height, 1, 17:32), height, 16)).^2;
+err.twoLayers = (twoLayersNir - reshape(phantom.clean(1:height, width, 17:32), height, 16)).^2;
+
+filename = ['materials-rnd-M', num2str(M), '-layers', num2str(layers), '-c', num2str(fileID)];
+
+save(filename, 'err', 'net', 'perf', 'mStatsPerm');
+
+end
+
---
+++ phantom/resultLoadErrorMaterialsCount.m
@@ -0,0 +1,33 @@
+function [errorClean, errorPent] = resultLoadErrorMaterialsCount(path)
+% [errorClean, errorPent] = resultLoadErrorPhantomSize(path) - if path is
+% not specified '/Volumes/G2/data/matlab-datasets/phantomSizeVersusNNSize/'
+% is taken.
+% Function loads whole directory with computed ANN. Expected filename of
+% each file is in format output-P${PHANTOM_ID}-M12-L${LAYERS_ID}-c${CYCLE}.mat
+% Loaded variable is only error matrix for one layer -> errorClean and for
+% twoLayers -> errorPent
+% Missing files are reported as warning message.
+
+    if nargin < 1
+        path = '/home/gimli/tmp/materialsVsLayers/';
+    end
+
+    errorClean = zeros(11,10,20,16);
+    errorPent = zeros(11,10,20,16);
+    
+    for m=10:10:110
+        for l=1:10
+            for c=1:20
+                filename = [path, 'materials-rnd-M', num2str(m), '-L', num2str(l), '-c', num2str(c), '.mat'];
+                if ~exist(filename, 'file')
+                    warning(['File does not exist: ', filename]);
+                    continue
+                end
+                load(filename, 'err');
+                
+                errorClean(m/10, l, c, :) = reshape(mean(err.oneLayer,1) / 20, 1,1,1,16);
+                errorPent(m/10, l, c, :) = reshape(mean(err.twoLayers) / 20,1,1,1,16);                
+            end
+        end
+    end
+end
